32b4b9ee646682181ae47ec9c339631ab1576715
aflorent44 <amelie.florent2024@campus-eni.fr>
2025-01-28 13:47:23 +0100

    remplacement des variables ville et habitant par message et utilisateur

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("coucou");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+let messages = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages });
+});
+
+app.post("/cities/create", (req, res) => {
+  const { message, auteur } = req.body;
+  messages.push({ message, auteur });
+  res.redirect("/");
+});
+app.listen(port, () => {
+  console.log("Le serveur tourne sur le port " + port);
+});
